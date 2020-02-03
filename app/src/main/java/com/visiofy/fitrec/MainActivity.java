55f620540acef6b877b8591b2ebbc0e2c970fbe0
Abhinav Gupta <abhiabhinav98@gmail.com>
2020-02-03 21:26:58 +0530

    Initial commit

---
+++ app/src/main/java/com/visiofy/fitrec/MainActivity.java
@@ -0,0 +1,27 @@
+package com.visiofy.fitrec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)  {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        try {
+            TimeUnit.SECONDS.sleep(5);
+            Intent i = new Intent(MainActivity.this, activity_home.class);
+            startActivity(i);
+        }
+        catch(Exception ex){
+
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/visiofy/fitrec/activity_home.java
@@ -0,0 +1,51 @@
+package com.visiofy.fitrec;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class activity_home extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    //mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    //mTextMessage.setText("Dashboard");
+                    return true;
+                case R.id.navigation_recommender:
+                    //mTextMessage.setText("Recommendation");
+                    return true;
+                case R.id.navigation_settings:
+                   // mTextMessage.setText("Settings");
+                    return true;
+                case R.id.navigation_profile:
+                    //mTextMessage.setText("Profile");
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
